8e567e4c40d227d5217ce196979d511759ec25f6
InavL <ATB1998@gmail.com>
2019-09-29 19:26:24 +0200

    nu jvalar

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,12 @@
 function hamburger(x) {
     x.classList.toggle("change");
-  } 
+}
+
+function hamMenu1() {
+    var x = document.getElementById("pop");
+    if (x.className === "popupMenu") {
+        x.className += " responsive";
+    } else {
+        x.className = "popupMenu";
+    }
+}

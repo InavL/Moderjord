c9d4483fff7fc1d9fd0c6a84513a31e375a8a7b1
InavL <ATB1998@gmail.com>
2019-09-29 11:15:25 +0200

    fixes

--- js/bildspel.js
+++ js/bildspel.js
@@ -1,10 +1,7 @@
-
 var hej = new Boolean(true);
-var index=0;
-bildSpel()
+var index = 0;
 
-
- function bildSpel() {
+function bildspel() {
   var i;
   var bilder = document.getElementsByClassName("bildspel");
   for (i = 0; i < bilder.length; i++) {
@@ -12,26 +9,27 @@ bildSpel()
   }
   index++;
 
-  if (index > bilder.length) {index = 1}
-  bilder[index-1].style.display = "block";
-  if(hej==true){
-  setTimeout(bildSpel, 4000);
-}
-  else{
-    
+  if (index > bilder.length) {
+    index = 1
+  }
+  bilder[index - 1].style.display = "block";
+  if (hej == true) {
+    setTimeout(bildSpel, 4000);
+  } else {
+
   }
 }
 
-$(document).ready(function() {
-  $("#stop").click(function(){
-    console.log( "ready!" );
-    hej= new Boolean(false);
+$(document).ready(function () {
+  $("#stop").click(function () {
+    console.log("ready!");
+    hej = new Boolean(false);
   });
+});
+$(document).ready(function () {
+  $("#start").click(function () {
+    console.log("ready!");
+    hej = new Boolean(true);
+    bildSpel();
   });
-  $(document).ready(function() {
-    $("#start").click(function(){
-      console.log( "ready!" );
-      hej= new Boolean(true);
-      bildSpel();
-    });
-    });
+});

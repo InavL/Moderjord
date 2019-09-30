33f15298d42542d6eb1654ec1f00b37d611d8dfa
4ndsil <46993210+4ndsil@users.noreply.github.com>
2019-09-30 15:41:26 +0200

    <3

--- js/bildspel.js
+++ js/bildspel.js
@@ -4,33 +4,38 @@ var index=0;
 function bildspel() {
   var i;
   var bilder = document.getElementsByClassName("bildspel");
+  var timer;
   for (i = 0; i < bilder.length; i++) {
     bilder[i].style.display = "none";
   }
+  if (hej == true) {
   index++;
-
+  var timer =setTimeout(bildspel,2000);
+  } 
   if (index > bilder.length) {
     index = 1
   }
   bilder[index - 1].style.display="block";
-  if (hej == true) {
-    setTimeout(bildspel, 1000);
-  } else {
-
-  }
 }
 
 $(document).ready(function () {
   $("#startStop").click(function () {
-    var knapp =  document.getElementsByClassName("startStop");
     if(hej==false){
     console.log("ready!");
     hej = new Boolean(true);
-    knapp.innerHTML ="Starta bildspel";
     bildspel();
-  }
-  else{
+  } else{
     hej = new Boolean(false);
+    clearTimeout(timer);
   }
+ });
 });
-});
+
+function change(){
+  var knapp =document.getElementById("startStop");
+  if(knapp.value=="Stoppa bildspel"){
+    knapp.value="Starta bildspel"; 
+}else{
+  knapp.value="Stoppa bildspel"; 
+}
+}
